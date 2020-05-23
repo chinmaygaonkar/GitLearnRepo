8e6211864131ad467c009866fcc75a5ac640c334
ChinmayGaonkar <cgaonkar7@gmail.com>
2020-05-24 01:07:10 +0530

    Initial Commit

---
+++ JAVA-Programs/Case.java
@@ -0,0 +1,12 @@
+public class Case {
+    private String model;
+    private String manufacturer;
+    private int ramSlots;
+    private int cardSlots;
+
+    public Case(String ename, int id1){
+        this.name=ename;
+        this.id=id1;
+    }
+    
+}
